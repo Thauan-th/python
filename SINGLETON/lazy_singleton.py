f89b64e6b79ea9442fd9c8ccb973690e8dc481ff
Thauan-th <thauanandre09@gmail.com>
2023-10-22 19:28:25 -0300

    lazy singleton

---
+++ SINGLETON/lazy_singleton.py
@@ -0,0 +1,21 @@
+class Singleton:
+    __instance = None
+
+    def __init__(self) -> None:
+        if self.__instance is None:
+            print('No instance')
+        else:
+            print(f'Instace: {id(self.get_instance())}')
+
+    @classmethod
+    def get_instance(cls):
+        if cls.__instance is None:
+            cls.__instance = Singleton()
+        return cls.__instance
+
+
+c1 = Singleton()  # No instance yet
+
+print(f'New instance: {Singleton.get_instance()}')  # Creating new instance
+
+c2 = Singleton()  # should have an instance
